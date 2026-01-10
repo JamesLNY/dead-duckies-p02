c51114bdf9d0889d2206022cc06a35cbf1447d19
James Lei <leijames325@gmail.com>
2026-01-10 08:09:04 -0500

    Socket proof of concept

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import SocketIO, join_room, emit
 
 app = Flask(__name__)
 app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+socketio = SocketIO(app)
 
-@app.get('/')
-def home_get():
+@app.get('/game')
+def game_get():
+  session["username"] = "user1" # TESTING
   return render_template("game.html")
 
-app.run(debug=True)
+@app.post('/lobby')
+def lobby_post():
+  session["room"] = request.args["room"]
+  return redirect("/game")
+
+@socketio.on('join')
+def handle_join(data):
+  username = session["username"]
+  room = session["room"]
+  join_room(room)
+  emit("message", f'{username} has entered room {room}', to=room)
+
+if __name__ == '__main__':
+  app.debug = True
+  socketio.run(app)
