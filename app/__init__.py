55e9eb4d166540877af8d2522139d3396d208a94
emaan asif <emaanasif438@gmail.com>
2026-01-12 18:50:50 -0500

    mrge conflict

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@
 
 from flask import Flask, render_template, request, session, redirect, flash, url_for
 from flask_socketio import SocketIO, join_room, emit
+import uuid
 
 app = Flask(__name__)
 app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
 socketio = SocketIO(app)
+
+lobbies = {}
 
 import auth
 app.register_blueprint(auth.bp) 
@@ -23,25 +26,52 @@ def check_authentification():
 
 @app.get('/')
 def home_get():
-  return "temp"
-
-@app.post('/lobby')
-def lobby_post():
-  session["room"] = request.args["room"]
   return redirect("/game")
 
-@app.get('/game')
+@app.get("/game")
 def game_get():
-  session["username"] = "user1" # TESTING
-  return render_template("game.html")
+    if "username" not in session:
+        flash("Please log in", "error")
+        return redirect(url_for("auth.login_get"))
+    if "room" not in session:
+        return redirect("/lobby")
+    return render_template("game.html", lobby_id=session["room"], username=session["username"])
 
-@socketio.on('join')
+
+@app.post("/create")
+def create_lobby():
+    lobby_id = str(uuid.uuid4()).replace("-", "")[:6].upper()  
+    session["room"] = lobby_id
+    if lobby_id not in lobbies:
+        lobbies[lobby_id] = []
+    return redirect("/game")
+
+@app.post("/join")
+def join_lobby():
+    lobby_id = request.form["room"].upper()
+    if lobby_id not in lobbies:
+        flash("Lobby does not exist", "error")
+        return redirect("/lobby")
+    session["room"] = lobby_id
+    return redirect("/game")
+
+@app.get("/lobby")
+def lobby_get():
+    return render_template("lobby.html")
+
 def handle_join(data):
-  username = session["username"]
-  room = session["room"]
-  join_room(room)
-  emit("message", f'{username} has entered room {room}', to=room)
+    global lobbies
+    lobby_id = data.get("lobby_id")
+    username = data.get("username")
+    if not lobby_id or not username:
+        return
+    if lobby_id not in lobbies:
+        lobbies[lobby_id] = []
+    if username not in lobbies[lobby_id]:
+        lobbies[lobby_id].append(username)
+    join_room(lobby_id)
+    emit("lobby_update", lobbies[lobby_id], room=lobby_id)
 
 if __name__ == '__main__':
-  app.debug = True
-  socketio.run(app)
+    app.debug = True
+    socketio.run(app)
