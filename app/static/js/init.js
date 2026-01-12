7a9945f50d7f3cd30576c0a6ce511b74e866b8a2
James Lei <leijames325@gmail.com>
2026-01-12 18:36:01 -0500

    Added standard population in init

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -3,7 +3,55 @@ import { getJson, overlay } from "./utility.js";
 const STARTING_MAP = await getJson("map.json")
 const RESOURCE_YIELDS = await getJson("resources.json")
 
-let map = {}
+// Temporary map example
+let map = [
+  [
+    {
+      "terrain_type": "plains",
+      "resource": null,
+      "improvements": [],
+      "unit": null,
+      "yield": {
+        "food": 1,
+        "production": 1
+      }
+    },
+    {
+      "terrain_type": "mountain",
+      "resource": "diamonds",
+      "improvements": ["mine"],
+      "unit": null,
+      "yield": {
+        "gold": 3,
+        "production": 4
+      }
+    }
+  ]
+]
+
+let storedResources = {
+  "science": 0,
+  "gold": 0,
+  "food": 0,
+  "production": 0,
+  "population": 0,
+  "iron": 0,
+  "horses": 0,
+  "niter": 0,
+  "coal": 0
+}
+
+let resourcesPerTurn = {
+  "science": 0,
+  "gold": 0,
+  "food": 0,
+  "production": 0,
+  "population": 0,
+  "iron": 0,
+  "horses": 0,
+  "niter": 0,
+  "coal": 0
+}
 
 function renderMap() {
   const map = document.getElementById("map")
@@ -26,4 +74,4 @@ function renderMap() {
 
 renderMap()
 
-export { map }
+export { map, storedResources, resourcesPerTurn }
