8099e3fa4a0deaf0adb00ffdb97c26d4b02e74bf
James Lei <leijames325@gmail.com>
2026-01-11 20:22:23 -0500

    Added design doc + helper function

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,38 +1,40 @@
 const map = document.getElementById("map")
 
 function clickTile(event) {
-  console.log(event.target.getAttribute("x") + ", " + event.target.getAttribute("y")) 
+  let x = event.target.parentNode.getAttribute("x")
+  let y = event.target.parentNode.getAttribute("y")
+}
+
+function overlay(x, y, link, rotation=0) {
+  const img = document.createElement('img');
+  img.style.position = "absolute";
+  img.style.left = 0;
+  img.style.top = 0;
+  img.src = `/static/images/${link}`;
+  img.style.width = "128px"
+  img.style.height = "112px"
+  if (rotation != 0) {
+    img.style.transform = `rotate(${rotation}deg)`;
+  }
+  img.onclick = clickTile
+  const div = document.querySelector(`div[x="${x}"][y="${y}"]`)
+  div.append(img)
 }
 
 function init() {
   for (let y = 0; y < 50; y++) {
     for (let x = 0; x < 50 - (y % 2); x++) {
-      const img = document.createElement('img');
-
-      img.setAttribute("x", x);
-      img.setAttribute("y", y);
-
-      img.style.position = "absolute"
-      img.style.left = `${x * 112 + y % 2 * 56}px`;
-      img.style.top = `${20 +   y * 95}px`;
-
-      img.src = "/static/images/Grassland.png"
-      img.style.width = "128px"
-      img.style.height = "112px"
-      img.style.transform = 'rotate(90deg)';
-
-      const overlay = img.cloneNode()
-      overlay.src = "/static/images/Grassland Hill.png"
-      overlay.style.transform = '';
-
-      img.onclick = clickTile;
-      overlay.onclick = clickTile;
-
-      // img.src = "https://static.wikia.nocookie.net/civilization/images/a/a5/Grassland_%28Civ6%29.png";
+      const div = document.createElement('div');
+      div.style.position = "absolute"
+      div.style.left = `${x * 112 + y % 2 * 56}px`;
+      div.style.top = `${20 +   y * 95}px`;
+      div.setAttribute("x", x);
+      div.setAttribute("y", y);
       
-      
-      map.append(img);
-      map.append(overlay);
+      map.append(div);
+
+      overlay(x, y, "Grassland.png", 90)
+      overlay(x, y, "Grassland Hill.png", 0)
     }
   }
 }
