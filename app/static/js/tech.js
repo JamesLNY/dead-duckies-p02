8650925efc6530eae5243cfd2bd07e4690d2990a
@Kiran1029 <kiranjsoemardjo@gmail.com>
2026-01-12 19:17:41 -0500

    completetech method

--- app/static/js/tech.js
+++ app/static/js/tech.js
@@ -1,6 +1,9 @@
 import { getJson } from "./utility.js";
+const socket = io();
 
 const TECHNOLOGIES = await getJson("technology.json")
+const researched = []
+const available = ["pottery", "husbandry", "mining", "sailing"]
 
 var techSidebar = document.getElementById("tech-sidebar");
 var infoSidebar = document.getElementById("info-sidebar");
@@ -21,4 +24,18 @@ infoSidebar.onclick = () => {
   techSidebar.classList.remove("opened");
 }
 
+function completeTech(tech) {
+    available.splice(available.indexOf(tech), 1)
+    researched.push(tech)
+    for (dependant in TECHNOLOGIES[tech]["tech_unlocks"]) {
+        if (TECHNOLOGIES[dependant]["tech_unlocks"].every(prereq => researched.includes(prereq))) { // adds each tech unlock only if each has had all prereqs researched
+            available.push(dependant)
+        }
+    }
+    socket.emit("tech_finished", {"technology_name": tech})
+}
+
+function isResearched(tech) {
+    return researched.includes(tech)
+}
 console.log(TECHNOLOGIES)
