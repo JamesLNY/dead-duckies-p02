bc9a1448fdf2bedfe634b7f5a97fa5ef81beb99f
James Lei <bestpsyduckever@gmail.com>
2026-01-13 20:15:09 -0500

    Some work on districts

--- app/static/js/construction.js
+++ app/static/js/construction.js
@@ -1,23 +1,25 @@
-import { getJson, getAdjacentTiles } from "./utility.js"
+import { getJson, getAdjacentTiles, consumeResource, overlay } from "./utility.js"
 import { map } from "./init.js"
 
 const IMPROVEMENTS = await getJson("improvements.json")
 const DISTRICTS = await getJson("districts.json")
 
-function build() {
+function buildImprovement() {
 
 }
 
 function buildDistrict(name, x, y) {
   let tile = map[y][x]
+  overlay(x, y, `districts/${name}.png`, 0, "improvement")
   const district = DISTRICTS[name]
-  consumeResource("production", 54)
+  consumeResource("production", district["production cost"])
   let adjacent = getAdjacentTiles(x, y)
 
-  for (let [key, value] of Object.entries(object)) {
+  for (let [key, value] of Object.entries(district["adjacency"])) {
     adjacent.forEach((tile) => {
+      console.log(tile)
       let count = 0;
-      if (tile["terrain_type"].includes(key)) count++
+      if (tile["terrain"].includes(key)) count++
       if (key == tile) count++
       if (key in tile["improvements"]) count++
       for (let [resource, amount] of Object.entries(tile)) {
@@ -27,4 +29,4 @@ function buildDistrict(name, x, y) {
   }
 }
 
-export { build }
+export { buildDistrict }
