0d823ef299ebde2628fb60bdb8debc70a0cbb6ab
James Lei <leijames325@gmail.com>
2026-01-14 22:02:23 -0500

    Integrated worker stuff

--- app/static/js/workers.js
+++ app/static/js/workers.js
@@ -1,41 +1,41 @@
 import { storedResources, TECHNOLOGIES, DISTRICTS, IMPROVEMENTS, map } from "./init.js";
 
-var workerButton = document.getElementById("worker-button")
-workerButton.disabled = true
+// var workerButton = document.getElementById("worker-button")
+// workerButton.disabled = true
 let workers = 0
 
-workerButton.onclick = () => {
-    let x = workerButton.getAttribute("x")
-    let y = workerButton.getAttribute("y")
-    if ("remove" in workerButton.classList) {
-        removeWorker(x, y)
-    }
-    else {
-        assignWorker(x, y)
-    }
-}
+// workerButton.onclick = () => {
+//     let x = workerButton.getAttribute("x")
+//     let y = workerButton.getAttribute("y")
+//     if ("remove" in workerButton.classList) {
+//         removeWorker(x, y)
+//     }
+//     else {
+//         assignWorker(x, y)
+//     }
+// }
 
-function updateWorkerButton(x, y) {
-    workerButton.disabled = false
-    workerButton.hidden = false
-    if (map[y][x]["worked"]) {
-        workerButton.innerHTML = "Remove Worker"
-        workerButton.classList.add("remove")
-        workerButton.classList.remove("assign")
-    }
-    else {
-        workerButton.innerHTML = "Assign Worker"
-        workerButton.classList.add("assign")
-        workerButton.classList.remove("remove")
-        workerButton.disabled = (storedResources["population"] <= workers)
-    }
-    workerButton.setAttribute("x", x)
-    workerButton.setAttribute("y", y)
-}
+// function updateWorkerButton(x, y) {
+//     workerButton.disabled = false
+//     workerButton.hidden = false
+//     if (map[y][x]["worked"]) {
+//         workerButton.innerHTML = "Remove Worker"
+//         workerButton.classList.add("remove")
+//         workerButton.classList.remove("assign")
+//     }
+//     else {
+//         workerButton.innerHTML = "Assign Worker"
+//         workerButton.classList.add("assign")
+//         workerButton.classList.remove("remove")
+//         workerButton.disabled = (storedResources["population"] <= workers)
+//     }
+//     workerButton.setAttribute("x", x)
+//     workerButton.setAttribute("y", y)
+// }
 
-function hideWorkerButton() {
-    workerButton.hidden = true
-}
+// function hideWorkerButton() {
+//     workerButton.hidden = true
+// }
 
 function assignWorker(x, y) {
     map[y][x]["worked"] = true
@@ -46,3 +46,5 @@ function removeWorker(x, y) {
     map[y][x]["worked"] = false
     workers--
 }
+
+export {assignWorker, removeWorker};
